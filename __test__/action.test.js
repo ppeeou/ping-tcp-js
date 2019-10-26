efd429bbe9eaee82e78bf8eecd6b1887f8993066
xodltus <xodltus@naver.com>
2019-10-27 02:33:45 +0900

    add test

--- __test__/action.test.js
+++ __test__/action.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const { ping, pingBackOff } = require('../lib');
 
 describe('Ping test', function () {
+  this.timeout(5000);
   it('ping', function (done) {
     ping('google.com', 80)
       .then(assert.ok)
@@ -13,6 +14,12 @@ describe('Ping test', function () {
       host: 'google.com',
       port: 80
     })
+      .then(assert.ok)
+      .then(done)
+  });
+
+  it('ping url', function (done) {
+    ping('https://google.com')
       .then(assert.ok)
       .then(done)
   });
@@ -33,4 +40,11 @@ describe('Ping test', function () {
       .then(assert.ok)
       .then(done)
   });
+
+  it('ping backoff', function (done) {
+    pingBackOff('https://google.com', 5, 10)
+      .then(assert.ok)
+      .then(done)
+  });
+
 })
